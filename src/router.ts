b854aa24d302fe087bb1c39055a5ec4516c0afd1
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-01-24 14:30:58 -0300

    validacao de filmes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
 import createMovie from "./contollers/Movie";
 import validate from "./middlewares/handleValidaton";
+import movieCreateValidation from "./middlewares/movieValidation";
 
 const router = Router()
 
 export default router.get('/teste', (req: Request, res: Response) => {
     res.status(200).send('Api is working!!!')
-}).post('/movie', validate, createMovie)
+}).post('/movie', movieCreateValidation, validate, createMovie)
