31ca55ca28056892511c8c90bb9bb017ba94e584
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-01-24 10:56:51 -0300

    configurado morgan

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import config from 'config';
 import router from './router';
 import db from '../config/db';
 import Logger from '../config/logger';
+import morganMiddleware from './middlewares/morganMiddleware';
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json())
 
 const PORT = config.get<number>('port')
 
+app.use(morganMiddleware)
 app.use('/api/', router)
 
 app.listen(PORT, async () => {
